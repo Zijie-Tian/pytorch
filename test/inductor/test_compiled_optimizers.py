7144341fa1d2d39ad1ba93de7d1dc553510e4605
Peter Bell <peterbell10@live.co.uk>
2024-07-19 22:24:02 +0100

    [inductor] Refactor fusion of inplace operations
    Resubmit of #128979
    
    `WeakDep`s force readers to have complete before a mutation overwrites the
    buffer, but we want to allow fusions to occur for inplace mutations where the
    same index is read and written.
    
    Currently this is achieved by:
    1. Identifying the buffers used by the mutating op in its `dep_closure`
    2. Not creating `WeakDep`s for buffers in the `dep_closure`
    3. Fixing up any bad fusions that might occur by an extra check in `can_fuse_vertical`
    
    So we are first over-agressive in removing `WeakDep`, then add an ad-hoc fixup.
    
    This PR instead emits all `WeakDep`s and adds a `fusable_weak_dep` check to
    `can_fuse_vertical` which selectively allows inplace operation to fuse.
    
    ghstack-source-id: 1d469d7f8f46e45e7f5d7ac5a6d278ae20f51813
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130835

--- test/inductor/test_compiled_optimizers.py
+++ test/inductor/test_compiled_optimizers.py
@@ -124,8 +124,8 @@ KERNEL_COUNT_OVERRIDES = {
     "test_adadelta_foreach_weight_decay_cpu": 12,
     "test_sgd_foreach_momentum_weight_decay_cpu": 16,
     "test_sgd_foreach_momentum_nesterov_weight_decay_cpu": 16,
-    "test_sgd_momentum_dampening_foreach_cuda": 5,
-    "test_sgd_momentum_foreach_cuda": 5,
+    "test_sgd_momentum_dampening_foreach_cuda": 6,
+    "test_sgd_momentum_foreach_cuda": 6,
     "test_sgd_weight_decay_maximize_cuda": 4,
     "test_sgd_weight_decay_maximize_cpu": 4,
     "test_sgd_momentum_weight_decay_foreach_cuda": 2,
@@ -133,16 +133,12 @@ KERNEL_COUNT_OVERRIDES = {
     "test_sgd_cuda": 4,
     "test_sgd_cpu": 4,
     "test_rmsprop_tensor_lr_capturable_foreach_cuda": 4,
-    "test_adagrad_initial_accumulator_value_weight_decay_foreach_cuda": 3,
-    "test_adagrad_lr_decay_weight_decay_foreach_cuda": 3,
-    "test_adagrad_weight_decay_foreach_cuda": 3,
-    "test_adagrad_weight_decay_maximize_foreach_cuda": 3,
     "test_adagrad_tensor_lr_cpu": 6,
     "test_adagrad_tensor_lr_cuda": 6,
     "test_adamax_tensor_lr_weight_decay_capturable_cuda": 6,
-    "test_asgd_tensor_lr_weight_decay_maximize_capturable_cuda": 8,
+    "test_asgd_tensor_lr_weight_decay_maximize_capturable_cuda": 5,
     "test_asgd_tensor_lr_weight_decay_maximize_capturable_foreach_cuda": 4,
-    "test_nadam_tensor_lr_weight_decay_momentum_decay_decoupled_weight_decay_capturable_cuda": 9,
+    "test_nadam_tensor_lr_weight_decay_momentum_decay_decoupled_weight_decay_capturable_cuda": 6,
     "test_nadam_tensor_lr_weight_decay_momentum_decay_decoupled_weight_decay_capturable_foreach_cuda": 3,
     "test_radam_tensor_lr_capturable_weight_decay_decoupled_weight_decay_cuda": 6,
     "test_radam_tensor_lr_capturable_weight_decay_decoupled_weight_decay_foreach_cuda": 7,
@@ -161,13 +157,13 @@ KERNEL_COUNT_OVERRIDES = {
 KERNEL_COUNTS = {
     Adam: KernelCounts(multitensor=2, singletensor=8),
     AdamW: KernelCounts(multitensor=2, singletensor=8),
-    NAdam: KernelCounts(multitensor=2, singletensor=11),
+    NAdam: KernelCounts(multitensor=2, singletensor=8),
     Rprop: KernelCounts(multitensor=2, singletensor=8),
     RMSprop: KernelCounts(multitensor=2, singletensor=8),
     Adadelta: KernelCounts(multitensor=2, singletensor=8),
     Adagrad: KernelCounts(multitensor=2, singletensor=8),
     SGD: KernelCounts(multitensor=1, singletensor=8),
-    ASGD: KernelCounts(multitensor=2, singletensor=11),
+    ASGD: KernelCounts(multitensor=2, singletensor=8),
     RAdam: KernelCounts(multitensor=2, singletensor=8),
     Adamax: KernelCounts(multitensor=2, singletensor=8),
 }
@@ -584,7 +580,7 @@ class CompiledOptimizerTests(TestCase):
     test_adagrad_recompile = make_recompile_test(Adagrad, lr=0.01)
     test_asgd_recompile_default = make_recompile_test(ASGD, lr=0.01)
     test_asgd_recompile_single = make_recompile_test(
-        ASGD, kernel_count=11, lr=0.01, foreach=False
+        ASGD, kernel_count=8, lr=0.01, foreach=False
     )
     test_asgd_recompile_foreach = make_recompile_test(ASGD, lr=0.01, foreach=True)
     test_sgd_recompile_single = make_recompile_test(
